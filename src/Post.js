2c43bf744efcd7335803e5ad59438cd81111b32c
Naimish <naimishbbhuva@gmail.com>
2022-07-27 18:28:09 +0530

    comment function added, one bug remaining

--- src/Post.js
+++ src/Post.js
@@ -1,8 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Post.css";
 import Avatar from "@material-ui/core/Avatar";
+import { db } from "./firebase";
+import firebase from "firebase";
 
-function Post({ username, caption, imageUrl }) {
+function Post({ postId, user, username, caption, imageUrl }) {
+    const [comments, setComments] = useState([]);
+    const [comment, setComment] = useState("");
+
+    useEffect(() => {
+        let unsubscribe;
+        if (postId) {
+            unsubscribe = db
+                .collection("posts")
+                .doc(postId)
+                .collection("comments")
+                .orderBy("timestamp", "desc")
+                .onSnapshot((snapshot) => {
+                    setComments(snapshot.docs.map((doc) => doc.data()));
+                });
+        }
+        return () => {
+            unsubscribe();
+        };
+    }, [postId]);
+    const postComment = (event) => {
+        event.preventDefault();
+        db.collection("posts").doc(postId).collection("comments").add({
+            text: comment,
+            username: user.displayName,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+        });
+        setComment("");
+    };
     return (
         <div className="post">
             <div className="post-header">
@@ -15,6 +45,39 @@ function Post({ username, caption, imageUrl }) {
             <h4 className="post-text">
                 <strong>{username}:</strong> {caption}
             </h4>
+
+            {
+                <div className="post-comments">
+                    {comments.map((comment) => {
+                        return (
+                            <p>
+                                <strong>{comment.username}</strong>{" "}
+                                {comment.text}
+                            </p>
+                        );
+                    })}
+                </div>
+            }
+
+            <form className="post-commentBox">
+                <input
+                    className="post-input"
+                    type="text"
+                    placeholder="Add a comment..."
+                    value={comment}
+                    onChange={(e) => {
+                        setComment(e.target.value);
+                    }}
+                />
+                <button
+                    disabled={!comment}
+                    className="post-button"
+                    type="submit"
+                    onClick={postComment}
+                >
+                    Post
+                </button>
+            </form>
         </div>
     );
 }
